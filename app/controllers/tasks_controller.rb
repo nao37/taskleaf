d9a4dfa96cd02d4cd1268ad60a28b2ed44457172
渡辺直 <w.nao.gh37@gmail.com>
2019-07-28 22:03:49 +0900

    create action 作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,5 +9,18 @@ class TasksController < ApplicationController
   end
 
   def new
+    @task = Task.new
+  end
+
+  def create
+    task = Task.new(task_params)
+    task.save!
+    redirect_to tasks_path, notice: 'タスク「#{task.name}」を登録しました。'
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description)
   end
 end
