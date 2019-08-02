c6513eff2224527ed7568d2e1cbdacfb9a14020e
渡辺直 <w.nao.gh37@gmail.com>
2019-08-02 13:19:40 +0900

    before_validation 追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,8 +2,15 @@ class Task < ApplicationRecord
   validates :name, presence: true
   validates :name, length: {maximum: 30}
   validate :validate_name_not_including_comma
+  before_validation :set_description_if_none
+
+  private
 
   def validate_name_not_including_comma
     errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
+  end
+
+  def set_description_if_none
+    self.description = '詳細なし' if description.blank?
   end
 end
