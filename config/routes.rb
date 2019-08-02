94caf5809d4d476620309ed2cfdc715480b74a12
渡辺直 <w.nao.gh37@gmail.com>
2019-08-02 14:11:52 +0900

    routes 修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
   namespace :admin do
-    get 'users/new'
-    get 'users/edit'
-    get 'users/index'
-    get 'users/show'
+    resources :users
   end
-  resources :tasks
 
   root to: 'tasks#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :tasks
 end
