cc1cf924cdfc61415250a58b40175cb517ede44b
渡辺直 <w.nao.gh37@gmail.com>
2019-08-02 14:59:13 +0900

    CRUD 処理を追記

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -13,12 +13,30 @@ class Admin::UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to admin_user_path(@user), notice: "ユーザー「#{@user.name}」を更新しました。"
+    else
+      render :edit
+    end
   end
 
   def index
+    @users - User.all
   end
 
   def show
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_path, notice: "ユーザー「#{@user.name}」を削除しました。"
   end
 
   private
